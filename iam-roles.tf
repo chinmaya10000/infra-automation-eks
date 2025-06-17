96ed06cffe6829aebae02d9bd1d7931693accdf3
chinmaya <chinmaya@gmail.com>
2025-06-17 14:58:29 +0530

    fix

--- iam-roles.tf
+++ iam-roles.tf
@@ -16,7 +16,7 @@ locals {
 resource "aws_iam_role" "external-admin" {
     name = "external-admin"
 
-    assume_role_policy = jsondecode({
+    assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
             {

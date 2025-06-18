0d7fb3ff44fe0e4ecaa11f90600cf5ff3332d32a
chinmaya <chinmaya@gmail.com>
2025-06-18 23:26:29 +0530

    initial commit

--- iam-roles.tf
+++ iam-roles.tf
@@ -1,92 +1,92 @@
 locals {
   aws_k8s_role_mapping = [
     {
-      rolearn = aws_iam_role.external-admin.arn
+      rolearn  = aws_iam_role.external-admin.arn
       username = "admin"
-      groups = ["none"]
+      groups   = ["none"]
     },
     {
-      rolearn = aws_iam_role.external-developer.arn
+      rolearn  = aws_iam_role.external-developer.arn
       username = "developer"
-      groups = ["none"]
+      groups   = ["none"]
     }
   ]
 }
 
 resource "aws_iam_role" "external-admin" {
-    name = "external-admin"
+  name = "external-admin"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Sid = ""
-                Principal = {
-                    AWS = var.user_for_admin_role
-                }
-            }
-        ]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          AWS = var.user_for_admin_role
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy" "external-admin-policy" {
-    name = "external-admin-policy"
-    description = "Policy for external admin"
+  name        = "external-admin-policy"
+  description = "Policy for external admin"
 
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = ["eks:DescribeCluster"]
-                Effect = "Allow"
-                Resource = "*"
-            }
-        ]
-    })
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["eks:DescribeCluster"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "external-admin-policy-attach" {
-    role = aws_iam_role.external-admin.name
-    policy_arn = aws_iam_policy.external-admin-policy.arn
+  role       = aws_iam_role.external-admin.name
+  policy_arn = aws_iam_policy.external-admin-policy.arn
 }
 
 resource "aws_iam_role" "external-developer" {
-    name = "external-developer"
+  name = "external-developer"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Sid = ""
-                Principal = {
-                    AWS = var.user_for_developer_role
-                }
-            }
-        ]
-    })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          AWS = var.user_for_developer_role
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy" "external-developer-policy" {
-    name = "external-developer-policy"
-    description = "Policy for external developer"
+  name        = "external-developer-policy"
+  description = "Policy for external developer"
 
-    policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = ["eks:DescribeCluster"]
-                Effect = "Allow"
-                Resource = "*"
-            }
-        ]
-    })
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["eks:DescribeCluster"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "external-developer-policy-attach" {
-    role = aws_iam_role.external-developer.name
-    policy_arn = aws_iam_policy.external-developer-policy.arn
+  role       = aws_iam_role.external-developer.name
+  policy_arn = aws_iam_policy.external-developer-policy.arn
 }
--- kube-resources.tf
+++ kube-resources.tf
@@ -1,84 +1,84 @@
 provider "kubernetes" {
-    host = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-    exec {
-        api_version = "client.authentication.k8s.io/v1beta1"
-        command = "aws"
-        args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 resource "kubernetes_namespace" "solar-system" {
-    metadata {
-      name = "solar-system"
-    }
+  metadata {
+    name = "solar-system"
+  }
 }
 
 resource "kubernetes_role" "namespace_viewer" {
-    metadata {
-        name = "namespace-viewer"
-        namespace = "solar-system"
-    }
+  metadata {
+    name      = "namespace-viewer"
+    namespace = "solar-system"
+  }
 
-    rule {
-        api_groups = [""]
-        resources = ["pods", "services", "configmaps", "secrets", "persistentvolumes"]
-        verbs = ["get", "list", "watch", "describe"]
-    }
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "services", "configmaps", "secrets", "persistentvolumes"]
+    verbs      = ["get", "list", "watch", "describe"]
+  }
 
-    rule {
-        api_groups = ["apps"]
-        resources = ["deployments", "daemonsets", "statefulsets"]
-        verbs = ["get", "list", "watch", "describe"]
-    }
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "daemonsets", "statefulsets"]
+    verbs      = ["get", "list", "watch", "describe"]
+  }
 }
 
 resource "kubernetes_role_binding" "namespace_viewer" {
-    metadata {
-        name = "namespace-viewer"
-        namespace = "solar-system"
-    }
+  metadata {
+    name      = "namespace-viewer"
+    namespace = "solar-system"
+  }
 
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind = "Role"
-        name = "namespace-viewer"
-    }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "namespace-viewer"
+  }
 
-    subject {
-        kind = "User"
-        name = "developer"
-        api_group = "rbac.authorization.k8s.io"
-    }
+  subject {
+    kind      = "User"
+    name      = "developer"
+    api_group = "rbac.authorization.k8s.io"
+  }
 }
 
 resource "kubernetes_cluster_role" "cluster_viewer" {
-    metadata {
-        name = "cluster-viewer"
-    }
+  metadata {
+    name = "cluster-viewer"
+  }
 
-    rule {
-        api_groups = [""]
-        resources = ["*"]
-        verbs = ["get", "list", "watch", "describe"]
-    }
+  rule {
+    api_groups = [""]
+    resources  = ["*"]
+    verbs      = ["get", "list", "watch", "describe"]
+  }
 }
 
 resource "kubernetes_cluster_role_binding" "cluster_viewer" {
-    metadata {
-        name = "cluster-viewer"
-    }
+  metadata {
+    name = "cluster-viewer"
+  }
 
-    role_ref {
-        kind = "ClusterRole"
-        name = "cluster-viewer"
-        api_group = "rbac.authorization.k8s.io"
-    }
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-viewer"
+    api_group = "rbac.authorization.k8s.io"
+  }
 
-    subject {
-        kind = "User"
-        name = "admin"
-        api_group = "rbac.authorization.k8s.io"
-    }
+  subject {
+    kind      = "User"
+    name      = "admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
 }

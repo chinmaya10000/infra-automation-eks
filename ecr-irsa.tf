c83aa46795f6da1eef492ae81eec8b5e49308e3e
chinmaya <chinmaya@gmail.com>
2025-06-21 11:23:47 +0530

    initial commit

---
+++ ecr-irsa.tf
@@ -0,0 +1,50 @@
+data "aws_iam_policy_document" "assume_irsa_role" {
+    statement {
+        actions = ["sts:AssumeRoleWithWebIdentity"]
+        effect = "Allow"
+
+        principals {
+            type = "Federated"
+            identifiers = [module.eks.oidc_provider_arn]
+        }
+
+        condition {
+            test = "StringEquals"
+            variable = "${replace(module.eks.oidc_provider_url, "https://", "")}:sub"
+            values = ["system:serviceaccount:solar-system:solar-app-sa"]
+        }
+    }
+}
+
+resource "aws_iam_role" "ecr_irsa_role" {
+    name = "${var.name}-ecr-irsa-role"
+    assume_role_policy = data.aws_iam_policy_document.assume_irsa_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_irsa_role_policy" {
+    role       = aws_iam_role.ecr_irsa_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+resource "kubernetes_service_account" "solar_app_sa" {
+  metadata {
+    name      = "solar-app-sa"
+    namespace = kubernetes_namespace.solar-system.metadata[0].name
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.ecr_irsa_role.arn
+    }
+  }
+
+  depends_on = [module.eks, aws_iam_role_policy_attachment.ecr_irsa_role_policy, kubernetes_namespace.solar-system]
+}
